c2d5b8fb32f526a56aa125fa6e685034e0bee0a3
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-09-26 16:39:20 +0200

    Initial commit

---
+++ item.gd
@@ -0,0 +1,8 @@
+extends Area2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	sprite_2d.hide()
+	gpu_particles_2d.emitting = true
